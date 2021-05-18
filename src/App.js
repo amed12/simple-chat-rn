c20240dbc32ec3a21718e2004ca341ebb1d973a1
Achmad Fathullah <fathullah@qiscus.co>
2021-05-18 21:50:02 +0700

    load svg

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {ILlogo} from './assets';
 
 export default function App() {
   return (
-    <View>
-      <Text style={styles.textAlign}>Hello world</Text>
+    <View style={styles.container}>
+      <ILlogo />
+      <Text>My Customer Services</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  textAlign: {
-    textAlign: 'center',
+  container: {
+    alignSelf: 'center',
   },
 });

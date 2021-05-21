0894cd7e4eb36d94c1d34cfbac850714af9e1518
Achmad Fathullah <fathullah@qiscus.co>
2021-05-21 15:46:42 +0700

    make ui home and profile like expected

--- src/pages/CustomerServices/index.js
+++ src/pages/CustomerServices/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {colors} from '../../utils';
 
 const CustomerServices = () => {
   return (
     <View style={styles.page}>
-      <Text>Halaman CS</Text>
-      <Text>On Going</Text>
+      <View style={styles.content}>
+        <Text>Halaman CS</Text>
+        <Text>On Going</Text>
+      </View>
     </View>
   );
 };
 const styles = StyleSheet.create({
-  page: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+  page: {
+    backgroundColor: colors.secondary,
+    flex: 1,
+  },
+  content: {
+    backgroundColor: colors.white,
+    flex: 1,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
 });
 export default CustomerServices;
